d282fa5d47c951f2c6f0f093b43d803df2046717
Luis Díaz Más <piponazo@plagatux.es>
2014-09-03 08:57:11 +0200

    Fixed CMake configuration for running tests

--- cmake/code_coverage_xml.cmake
+++ cmake/code_coverage_xml.cmake
@@ -11,22 +11,22 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE_XML _targetname _testrunner _outputname)
       MESSAGE(FATAL_ERROR "gcovr not found! Aborting...")
    ENDIF() # NOT GCOVR_PATH
 
-	ADD_CUSTOM_TARGET(${_targetname}
+   ADD_CUSTOM_TARGET(${_targetname}
 
-		# Run tests
-        COMMAND ${_testrunner} ${ARGV3} ${ARGV4} ${ARGV5} ${ARGV6}
+      # Run tests
+        COMMAND ${_testrunner} ${ARGV3} ${ARGV4} ${ARGV5}
 
-		# Running gcovr
-		COMMAND ${GCOVR_PATH} -x -r ".." -e "tests" -e "build" -o ${_outputname}.xml
+      # Running gcovr
+      COMMAND ${GCOVR_PATH} -x -r ".." -e "tests" -e "build" -o ${_outputname}.xml
         #COMMAND ${GCOVR_PATH} -x -r ${CMAKE_SOURCE_DIR} -e '${CMAKE_SOURCE_DIR}/scripts/'  -o ${_outputname}.xml
-		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-		COMMENT "Running gcovr to produce code coverage report."
-	)
+      WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+      COMMENT "Running gcovr to produce code coverage report."
+   )
 
-	# Show info where to find the report
-	ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
-		COMMAND ;
-		COMMENT "code coverage report saved in ${_outputname}.xml."
-	)
+   # Show info where to find the report
+   ADD_CUSTOM_COMMAND(TARGET ${_targetname} POST_BUILD
+      COMMAND ;
+      COMMENT "code coverage report saved in ${_outputname}.xml."
+   )
 
 ENDFUNCTION()
